23be232b0af7232871dd79da2d7a9540dd0fd548
parthsawho <128316512+parthsawho@users.noreply.github.com>
2024-06-04 11:49:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-// Example script.js for future use if needed
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("Website is fully loaded");
-  // Add any additional JavaScript here
-});
+
